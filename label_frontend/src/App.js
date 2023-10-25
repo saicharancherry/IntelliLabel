a207e4db8f20c943a9b194bd74c5ef4a124eb480
Anupama Ponukumati <anupamaponukumati@Anupamas-MacBook-Air.local>
2023-10-24 18:49:29 -0700

    Updated the Gallery page

--- label_frontend/src/App.js
+++ label_frontend/src/App.js
@@ -2,16 +2,12 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import './App.css';
 import HomePage from './components/HomePage';
 import DropImages from './components/DropImages';
 import NewProject from './components/NewProject';
-<<<<<<< Updated upstream
 import ImageViewer from './components/ObjectAnnotation';
-=======
 import ObjectAnnotation from './components/ObjectAnnotation';
 import GalleryPage from './components/Gallery';
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -21,12 +17,9 @@ function App() {
           <Route path='/' element={<HomePage/>}/>
           <Route path='/get-started' element={<DropImages/>}/>
           <Route path='/new-project' element={<NewProject/>}/>
-<<<<<<< Updated upstream
           <Route path='/object-annotation' element={<ImageViewer/>}/>
-=======
           <Route path='/object-annotatation' element={<ObjectAnnotation/>}/>
           <Route path='/gallery-page' element={<GalleryPage/>}/>
->>>>>>> Stashed changes
         </Routes>
       </BrowserRouter>
     </>
--- label_frontend/src/components/Gallery.js
+++ label_frontend/src/components/Gallery.js
@@ -1,23 +1,60 @@
 import React from 'react';
 import '../css/Gallery.css';
-import '../App.css';
+// import '../App.css';
 
+import { useState, useEffect } from 'react';
 
 function Gallery() {
-  const images = [
-    'https://via.placeholder.com/150',
-    'https://via.placeholder.com/150',
-    'https://via.placeholder.com/150',
-    // Add more image URLs here
-  ];
+  // const images = [
+  //   'https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=',
+  //   'https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg',
+  //   'https://media.istockphoto.com/id/1296913338/photo/sonoran-sunset.jpg?s=612x612&w=0&k=20&c=lGXd-vnDmH_bCnR53BNmwxsh3qn8MBLQoh5M926QAbY=',
+  //   // Add more image URLs here
+  // ];
+
+  const [uploadedImages, setUploadedImages] = useState([]);
+
+  useEffect(() => {
+    // Retrieve uploaded images from local storage
+    const uploadedImages = JSON.parse(localStorage.getItem('droppedImages') || '[]');
+    setUploadedImages(uploadedImages);
+  }, []);
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
 
   return (
-    <div className="gallery">
-      {images.map((image, index) => (
-        <img key={index} src={image} alt={`Image ${index}`} className="gallery-image" />
-      ))}
+    <div className="container">
+      <div className="menu">
+        <button>Actions</button>
+        <button>Community</button>
+      </div>
+      <div className="header">
+        <h1>Gallery Page</h1>
+      </div>
+      <div className="gallery-container">
+        <div className="gallery">
+          {uploadedImages.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Image ${index}`}
+              className="gallery-image"
+              onClick={() => handleImageClick(image)}
+            />
+          ))}
+        </div>
+        {selectedImage && (
+          <div className="selected-image">
+            <img src={selectedImage} alt="Selected Image" />
+          </div>
+        )}
+      </div>
     </div>
   );
-};
+}
 
 export default Gallery;
