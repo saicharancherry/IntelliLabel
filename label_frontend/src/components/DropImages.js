ea8e05ef5b4bbc3024b7dfd49293d6053461d779
saicharan peda <saicharan@saicharans-MacBook-Air.local>
2023-11-21 13:06:06 -0800

    Removed comments

--- label_frontend/src/components/DropImages.js
+++ label_frontend/src/components/DropImages.js
@@ -46,40 +46,6 @@ function DropImages() {
 
   }, []);
 
-
-//   const onDrop = useCallback(acceptedFiles => {
-//     acceptedFiles.forEach((file) => {
-//         const reader = new FileReader();
-//         reader.onload = () => {
-//             const image = new Image();
-//             image.src = reader.result;
-
-//             image.onload = () => {
-//                 // Desired size for YOLO
-//                 const targetWidth = 416;
-//                 const targetHeight = 416;
-
-//                 const canvas = document.createElement('canvas');
-//                 canvas.width = targetWidth;
-//                 canvas.height = targetHeight;
-//                 const ctx = canvas.getContext('2d');
-//                 ctx.drawImage(image, 0, 0, targetWidth, targetHeight);
-                
-//                 // You can then access the resized image in the desired format
-//                 const resizedImageDataUrl = canvas.toDataURL('image/jpeg');
-//                 // Store image as a data URL in local storage
-//                 const images = JSON.parse(localStorage.getItem(IMAGES_KEY) || '[]');
-//                 images.push(resizedImageDataUrl);
-//                 localStorage.setItem(IMAGES_KEY, JSON.stringify(images));
-//                 // Do something with the resized image data URL (e.g., send to a server, display in UI)
-//             };
-//         };
-//         reader.readAsDataURL(file);
-//     });
-//     setSelectedFiles(acceptedFiles);
-
-// }, []);
-
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
     accept: 'image/*',
